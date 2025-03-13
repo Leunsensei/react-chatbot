c16d625c820adb8da6df567f8100cd1b8a6fcf75
leunsensei <immanuelaluyi@gmail.com>
2025-03-13 21:18:53 +0100

    array fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,7 @@ import ChatMessages  from './components/ChatMessages';
 import './App.css'
 
 function App() { 
-  const [chatMessages, setChatMessages] = useState([{
-          message: 'hello chatbot',
-          sender: 'user',
-          id: '1',
-       }, {
-          message: 'hello human',
-          sender: 'robot',
-          id: '2',
-      }, {
-          message: 'get todays date',
-          sender: 'user',
-          id: '3',
-      }, {
-          message: 'Today is March 2',
-          sender: 'robot',
-          id: '4',
-      }]);
+  const [chatMessages, setChatMessages] = useState([]);
       // const [chatMessages, setChatMessages] = array;
       // const chatMessages = array[0];
       // const setChatMessages = array[1];
